6c510b32cf098a538a6f41379e004f16245b744f
Ada <yretenai@gmail.com>
2025-11-03 08:35:13 +0530

    build: fix builds on linux

--- src/trans.c
+++ src/trans.c
@@ -2,6 +2,16 @@
 //  https://git.sr.ht/~chronovore/titania
 //  SPDX-License-Identifier: MPL-2.0
 
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2 (1.57079632679489661923)
+#endif
+
 #include "structures.h"
 
 #include <titania_config_internal.h>
